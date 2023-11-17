0c31673347c1ea06e0ea189abc00f02740dd6a94
Vivek Nepal <vivekn936@gmail.com>
2023-11-17 13:25:26 +0545

    navbar done

--- src/components/dashboard_ui/navbar/navbar.tsx
+++ src/components/dashboard_ui/navbar/navbar.tsx
@@ -1,10 +1,27 @@
+"use client"
 import React from 'react'
+import { MdNotifications, MdOutlineChat, MdPublic, MdSearch } from 'react-icons/md'
+import "./_navbar.scss"
+import { usePathname } from 'next/navigation'
 
-type Props = {}
 
-const Navbar = (props: Props) => {
+const Navbar = () => {
+  const pathname = usePathname();
   return (
-    <div>Navbar</div>
+    <div className='navbar-container'>
+      <div className='nav-title'>{pathname.split("/").pop()}</div>
+      <div className="menu">
+        <div className="search">
+          <MdSearch/>
+          <input type="text" placeholder='Search...'/>
+        </div>
+        <div className="icons">
+          <MdOutlineChat size={20} />
+          <MdNotifications size={20} />
+          <MdPublic size={20} />
+        </div>
+      </div>
+    </div>
   )
 }
 
