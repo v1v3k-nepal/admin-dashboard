4cdd9cc84bcd86806bdb03c6567e379238d80b82
Vivek Nepal <vivekn936@gmail.com>
2023-11-21 00:48:33 +0545

    bug-fix: implemented redirection inside useEffect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 "use client"
 import { useRouter } from "next/navigation"
+import {useEffect} from "react"
 
 export default function Home() {
   const router = useRouter();
-  router.push("/dashboard")
+  useEffect(()=>{
+    router.push("/dashboard")
+  },[])
   return (
     <main>
       <p>Redirecting to Dashboard...</p>
