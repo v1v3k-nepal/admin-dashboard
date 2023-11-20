0377cdb6365fd89735ce357feeae12438e8efd99
Vivek Nepal <vivekn936@gmail.com>
2023-11-21 00:35:57 +0545

    implemented auto redirection from homepage to dashboard page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+"use client"
+import { useRouter } from "next/navigation"
+
 export default function Home() {
+  const router = useRouter();
+  router.push("/dashboard")
   return (
     <main>
-      This is a HomePage
+      <p>Redirecting to Dashboard...</p>
     </main>
   )
 }
