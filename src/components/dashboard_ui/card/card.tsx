f389c9cba3c91e2842b740fddc765fa66721e87b
Vivek Nepal <vivekn936@gmail.com>
2023-11-21 12:14:49 +0545

    framer motion implemented in cards

--- src/components/dashboard_ui/card/card.tsx
+++ src/components/dashboard_ui/card/card.tsx
@@ -1,10 +1,21 @@
+"use client"
 import React from 'react'
 import { MdSupervisedUserCircle } from 'react-icons/md'
+import {motion} from "framer-motion"
 import "./_card.scss"
 
 const Card = ({item}:{item:Com.TCardData}) => { 
   return (
-    <div className='card-container'>
+    <motion.div className='card-container'
+    initial={{scale: 0.6, opacity: 0, y: -50, x: 50}}
+    whileInView={{scale: 1, opacity: 1, y: 0, x: 0}}
+    transition={{
+      delay: 0.1*item.id,
+      x:{type: "spring", stiffness: 60},
+      y:{type: "spring", stiffness: 60},
+      ease: "easeIn"
+    }}
+    >
         <MdSupervisedUserCircle size={24}/>
         <div className="card-content">
             <h1 className='title'>{item.title}</h1>
@@ -16,7 +27,7 @@ const Card = ({item}:{item:Com.TCardData}) => {
                 </span>
             </div>
         </div>
-    </div>
+    </motion.div>
   )
 }
 
